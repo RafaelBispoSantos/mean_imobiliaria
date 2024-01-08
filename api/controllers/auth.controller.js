5248015908ff35f63e086a39c3915132d480d6e7
Rafael Bispo <rafael_bispo23@hotmail.com>
2024-01-07 22:26:51 -0300

    create and add OAuth component not error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import Jwt  from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -23,9 +23,47 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User ot found!"));
     const validPassword = bcryptjs.compareSync(passoword, validUser.passoword);
     if (!validPassword) return next(errorHandler(401, "Invalid password"));
-    const token = Jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-    const {passoword: pass, ...rest} = validUser._doc;
-    res.cookie('access_token', token,{httpOnly: true}).status(200).json(rest)
+    const token = Jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { passoword: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = user._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
+        email: req.body.email,
+        passoword: hashedPassword,
+        avatar: req.body.photo
+      });
+      await newUser.save();
+      const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+      const {passoword: pass, ...rest} = newUser._doc;
+      res.cookie('access_token', token, {httpOnly: true}).status(200).json(rest)
+     
+
+    }
   } catch (error) {
     next(error);
   }
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAtbujVXYa-l0kGwvq40U_YxRR8Aen-93I",
+  authDomain: "mern-estate-7f694.firebaseapp.com",
+  projectId: "mern-estate-7f694",
+  storageBucket: "mern-estate-7f694.appspot.com",
+  messagingSenderId: "397614047516",
+  appId: "1:397614047516:web:80c6476ff3152ce4705ad9"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
