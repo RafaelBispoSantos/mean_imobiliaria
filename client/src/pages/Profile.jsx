71d131b1273a12839e089635a3e648d95833c083
Rafael Bispo <rafael_bispo23@hotmail.com>
2024-01-14 23:05:23 -0300

    Complete show user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -20,7 +20,7 @@ import {
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { app } from "../firebase";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 function Profile() {
   const fileRef = useRef(null);
@@ -30,6 +30,8 @@ function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormdata] = useState({});
+  const [showListingsEror, setShowListingsEror] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   // // firebase storage
@@ -115,7 +117,7 @@ function Profile() {
   const handleSignOut = async () => {
     try {
       dispatch(signInSuccess());
-      const res = await fetch('/api/auth/signout');
+      const res = await fetch("/api/auth/signout");
       const data = await res.json();
       if (data.success === false) {
         dispatch(deleteUserFailure(data.message));
@@ -124,6 +126,21 @@ function Profile() {
       dispatch(deleteUserSuccess(data));
     } catch (error) {
       dispatch(deleteUserFailure(data.message));
+    }
+  };
+
+  const handlewShowListing = async () => {
+    try {
+      setShowListingsEror(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setFileUploadError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsEror(true);
     }
   };
   return (
@@ -184,7 +201,12 @@ function Profile() {
           {loading ? "Loading..." : "Update"}
         </button>
 
-        <Link className="bg-green-700 text-white rounded-lg p-3 uppercase hover:opacity-95 text-center" to="/create-listing">Create Listing</Link>
+        <Link
+          className="bg-green-700 text-white rounded-lg p-3 uppercase hover:opacity-95 text-center"
+          to="/create-listing"
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
@@ -201,6 +223,42 @@ function Profile() {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
+      <button onClick={handlewShowListing} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-700 mt-5">
+        {showListingsEror ? "Error showing listings" : ""}
+      </p>
+      {userListings &&
+        userListings.length > 0 &&
+        <div className=" flex flex-col gap-4">
+          <h1 className="text-center mt-7 text-3xl font-semibold">Your Listings</h1>
+            {userListings.map((listing) => (
+          <div 
+            key={listing._id}
+            className=" border rounded-lg p-3 flex justify-between items-center gap-4"
+          >
+            <Link to={`/listing/${listing._id}`}>
+              <img
+                src={listing.imageUrls[0]}
+                alt="listing cover"
+                className="h-16 w-16 object-contain"
+              />
+            </Link>
+            <Link  className="text-slate-700 font-semibold flex-1 hover:underline truncate " to={`/listing/${listing._id}`}>
+              <p >
+                {listing.name}
+              </p>
+            </Link>
+            <div className="flex flex-col items-center">
+              <button className="text-red-700 uppercase">Delete</button>
+              <button className="text-green-700 uppercase">Edit</button>
+              
+            </div>
+          </div>
+            ))}
+      </div>}
+      
     </div>
   );
 }
